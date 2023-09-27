1bdc3556d811f82dab6176426ac0bf5e8f00fa5e
m1n1song <ajohn96@naver.com>
2023-09-27 23:26:05 +0900

    230927-day30

---
+++ web-dev30/app.js
@@ -0,0 +1,10 @@
+let greetingText = 'Hi Jack';
+let age = 32
+// alert(greetingText);
+let array = [1,2,3];
+// alert(array[0]);
+let dict = {age:24,name:'Jack'};
+alert(dict['age']);
+dict['age'] = 25;
+alert(dict['age']);
+alert(dict['name']);
