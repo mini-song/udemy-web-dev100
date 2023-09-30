518fc428f26a337083416d687c5e39be9bff54d7
m1n1song <ajohn96@naver.com>
2023-09-30 13:09:31 +0900

    230930-day31

---
+++ web-dev31/app.js
@@ -0,0 +1,12 @@
+let greetingText = 'Hi Jack';
+let age = 32
+// alert(greetingText);
+let array = [1,2,3];
+// alert(array[0]);
+let dict = {age:24,name:'Jack'};
+
+function calculateAge(age){
+    let calculateAge = age -18
+    return calculateAge;
+}
+alert(calculateAge(age));
