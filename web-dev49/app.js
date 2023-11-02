45e1dc71f860f7049af446285599149efa7d278b
m1n1song <ajohn96@naver.com>
2023-11-03 01:12:09 +0900

    231103-day49

---
+++ web-dev49/app.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded());
+
+app.get('/currenttime', function(req,res){
+    res.send('<h1>' + new Date().toISOString() + '</h1>')
+});
+
+app.get('/',function(req,res){
+    res.send('<form action="/store-user" method="POST"><label>Your_name : </label><input type="text" name="username"><button>submit</button></form>');
+});
+
+app.post('/store-user',function(req,res){
+    const username = req.body.username;
+
+    const filePath = path.join(__dirname,'data','users.json');
+
+    const fileData = fs.readFileSync(filePath);
+    const existingUsers = JSON.parse(fileData);
+
+    existingUsers.push(username);
+
+    fs.writeFileSync(filePath,JSON.stringify(existingUsers));
+    res.send('<h1>Username stored</h1>')
+}
+)
+app.get('/users',function(req,res){
+    const filePath = path.join(__dirname,'data','users.json');
+    const fileData = fs.readFileSync(filePath);
+    const existingUsers = JSON.parse(fileData);
+    let responseData ='<ul>';
+    for (const user of existingUsers){
+        responseData += '<li>' + user + '</li>'
+    }
+    responseData +='</ul>';
+    res.send(responseData);
+});
+
+app.listen(3000);
